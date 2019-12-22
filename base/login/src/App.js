9ae6cf7e84aad58f5f557da83957bdeb1623490b
Bogdan Bosca <bogdan.bucsa@travelport.com>
2019-12-22 14:09:55 +0200

    base(login): add router

--- base/login/src/App.js
+++ base/login/src/App.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import './App.css';
+import Home from './pages/Homepage';
 import Login from './pages/LoginPage';
+import NotFound from './pages/NotFound';
+import {Router} from '@reach/router';
 
 function App() {
     return (
-        <div>
-            hello world
-            <Login/>
-        </div>
+        <Router>
+            <Home path={"/"}/>
+            <Login path={"/login"}/>
+            <NotFound path={"*"}/>
+        </Router>
     );
 }
 
---
+++ base/login/src/pages/Homepage.jsx
@@ -0,0 +1,4 @@
+import React from 'react';
+import {Link} from '@reach/router';
+
+export default () => <div>Home page <Link to={'/login'}>go to login</Link></div>
