9b330e5708a5d63720651ef11c8aa2ba2fe32174
Bogdan Bosca <bogdan.bucsa@travelport.com>
2020-01-02 06:26:59 +0200

    add new ci

--- base64/src/init/routes.js
+++
@@ -1,9 +0,0 @@
-const aboutController = require('../controllers/about');
-const loginController = require('../controllers/login');
-const authenticateController = require('../controllers/authenticate');
-
-module.exports = (app) => {
-  app.get('/about', aboutController.get);
-  app.post('/login', loginController.post);
-  app.post('/authenticate', authenticateController.post);
-};
